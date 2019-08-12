cfd85dbc987e3d4757d931e9e20252ec5ab46114
edyson10 <edysonfabian@hotmail.com>
2019-08-11 22:50:41 -0500

    first commit

---
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.notas;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+        setInitialFragment();
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_content, fragment).commit();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new FragmentInicio();
+                break;
+        }
+        replaceFragment(fragment);
+        return true;
+    }
+
+    private void setInitialFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_content, new FragmentInicio());
+        fragmentTransaction.commit();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_content, fragment);
+        fragmentTransaction.commit();
+    }
+}
